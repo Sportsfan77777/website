d31aa5d2624fe1da14529eb859cc6988b84b3cb4
Sportsfan77777 <mhammer44444@gmail.com>
2015-08-20 23:50:03 -0700

    first commit to script for toolbar management, but so far not used here

---
+++ manage_toolbar.py
@@ -0,0 +1,73 @@
+"""
+finds a toolbar in a set of globbed html files
+and updates it with the current toolbar that is stored in
+'*****_toolbar.txt' where '*****' is the name of the toolbar
+
+1st argument is the name of the toolbar (required)
+"""
+
+import os
+import shutil
+import sys
+import glob
+
+name = "the name of the toolbar "
+
+if len(sys.argv) <= 1:
+    print "   *** Must have the name of the toolbar as the first argument ***"
+    exit()
+else:
+    name = sys.argv[1] # 1st Argument
+
+#### The argument is supplied ####
+
+print "Updating Toolbar named '%s'" % name
+toolbar_fn = "%s_toolbar.txt" % name
+
+# (1) Get .html files
+html_fns = glob.glob("*.html")
+
+# (2) Locate the lines starting and ending the toolbar in each file
+target_start = "<!-- start %s toolbar -->" % name
+target_end = "<!-- end %s toolbar -->" % name
+
+toolbar_f = open(toolbar_fn, "r") # will read from toolbar
+
+for html_fn in html_fns:
+    tmp_fn = "%s.tmp" % html_fn
+    f = open(html_fn, "r") # Read to this file
+    tmp_f = open(tmp_fn, "w") # Write to this file (mv it to old file later)
+
+    in_toolbar = False
+
+    # Read line by line
+    for line in f:
+        if in_toolbar:
+            # If in toolbar, do not copy (instead, write from a different file -- see below)
+            #pass
+            # Check if this is the end of the toolbar
+            if target_end in line:
+                # Unmark 'in_toolbar'
+                in_toolbar = False
+                # Write this line
+                tmp_f.write(line)
+        else:
+            # If not in toolbar, copy line to tmp
+            tmp_f.write(line)
+            # Check if this is the start of the toolbar
+            if target_start in line:
+                # Mark 'in_toolbar'
+                in_toolbar = True
+                # Then, copy toolbar from .txt file to .html.tmp file
+                for toolbar_line in toolbar_f:
+                    tmp_f.write(toolbar_line)
+
+    # Move tmp file to old file name
+    shutil.move(tmp_fn, html_fn) # move(src, dst)
+
+    # Close Files
+    f.close()
+    tmp_f.close()
+
+toolbar_f.close()
+
